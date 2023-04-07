c66e9cf998da61dadff59b92562df1058aa140bb
unknown <espinabrian@gmail.com>
2023-04-07 21:14:21 +0800

    removed input editable components

--- src/components/SessionFormFields/FormControl.jsx
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-const FormControlWrap = styled.div`
-  display: flex;
-`;
-export default function FormControl({ children }) {
-  return <FormControlWrap>{children}</FormControlWrap>;
-}
